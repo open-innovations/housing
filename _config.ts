4486fb96bb6dd9af6aa3fb4f003649ace8625e17
luke-strange <92686634+luke-strange@users.noreply.github.com>
2024-08-30 09:29:20 +0100

    Update version of Lume

--- _config.ts
+++ _config.ts
@@ -12,7 +12,7 @@ import json from "lume/core/loaders/json.ts";
 import { duckDbLoader } from "@dringtech/lume-duck";
 
 // Importing the OI Lume charts and utilities
-import oiViz from "https://deno.land/x/oi_lume_viz@v0.15.12/mod.ts";
+import oiViz from "https://deno.land/x/oi_lume_viz@v0.16.0/mod.ts";
 import autoDependency from "https://deno.land/x/oi_lume_utils@v0.4.0/processors/auto-dependency.ts";
 import csvLoader from "https://deno.land/x/oi_lume_utils@v0.4.0/loaders/csv-loader.ts";
 
